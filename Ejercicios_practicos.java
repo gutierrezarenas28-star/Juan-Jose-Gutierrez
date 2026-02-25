e843d0da1b82faa117b54557c90815d0e80d98c5
gutierrezarenas28-star <gutierrezarenas28@gmail.com>
2026-02-24 21:09:38 -0500

    Update Ejercicios_practicos.java

--- Ejercicios_practicos.java
+++ Ejercicios_practicos.java
@@ -38,6 +38,30 @@ public class Ejercicios_practicos {
         }
         
 
+        //placa de un vehiculo
+
+        String placa="BX4-76I";
+        int añoFabricacion=2024;
+        int cilindraje=1200;
+        double precioCompra=94000000;
+        char color= 'A';
+        boolean soat=true;
+
+        System.out.println("Placa: "+ placa+
+            "Año Fabricacion: "+ añoFabricacion+
+            "Cilindraje: "+ cilindraje+
+            "Precio Compra: "+ precioCompra+
+            "Color: "+ color+
+            "Soat: "+ soat
+        );
+
+
+        //conversor de unidades
+        double tazas=2;
+        
+
+
+
 
     }
     
